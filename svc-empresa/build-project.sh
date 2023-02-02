49051e466961b2ec21a1845f45aab5a89e662faf
mmendespro <marcelo.mendes.gomes@gmail.com>
2023-02-02 16:28:36 -0300

    improve scripts

--- svc-empresa/build-project.sh
+++ svc-empresa/build-project.sh
@@ -3,12 +3,12 @@
 # build the app
 mvn clean package
 # stop container
-docker stop departamento-service-jvm
+docker stop empresa-service-jvm
 # remove container
-docker rm departamento-service-jvm
+docker rm empresa-service-jvm
 # remove container image
-docker rmi spring/departamento-service-jvm:latest
+docker rmi spring/empresa-service-jvm:latest
 # build container image
-docker build -f Dockerfile.jvm -t spring/departamento-service-jvm .
+docker build -f Dockerfile.jvm -t spring/empresa-service-jvm .
 # run container
-#docker run -id --network=trace-net --name departamento-service-jvm -p 8081:8081 spring/departamento-service-jvm
+docker run -id --network=trace-net -e LOGGING_URL=http://openzipkin:9411 --name empresa-service-jvm -p 8080:8080 spring/empresa-service-jvm
