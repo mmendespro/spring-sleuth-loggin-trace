e8edcfbbc36bd42a4a26b1e49ffa796fb208df99
mmendespro <marcelo.mendes.gomes@gmail.com>
2023-02-02 08:33:55 -0300

    improve code

---
+++ svc-empregado/build-project.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# build the app
+mvn clean package
+# stop container
+docker stop empregado-service-jvm
+# remove container
+docker rm empregado-service-jvm
+# remove container image
+docker rmi spring/empregado-service-jvm:latest
+# build container image
+docker build -f Dockerfile.jvm -t spring/empregado-service-jvm .
+# run container
+docker run -id --network=trace-net --name empregado-service-jvm -p 8080:8080 spring/empregado-service-jvm
